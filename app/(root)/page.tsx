b56767e2a172ca846b20839a9a6cfe17df1d99d8
KEshav <keshavsingh7744@gmail.com>
2025-11-16 18:14:40 +0530

    Update dependencies in package.json and package-lock.json; add Radix UI components, query-string, react-chartjs-2, and react-countup. Refactor layout and page components to integrate new UI elements and improve structure. Fix typo in TotalBalanceBoxProps interface definition.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
+
+  const loggedIn = {firstName: "Keshav" , lastName : "Singh" , email:"kissu@gmail.com"};
+ 
   return (
-    <div>Home</div>
+    <section className='home'>
+
+      < div className='home-content'>
+
+        <header className='home-header'>
+          <HeaderBox
+            type = "greeting"
+            title = "Welcome"
+            user = {loggedIn?.firstName || "Guest"}
+            subtext = "Access and manage your account and transactions efficiently"
+          />
+         <TotalBalanceBox
+          accounts = {[]}
+          totalBanks = {1}
+          totalCurrentBalance = {1250}
+         />
+        </header>
+        recent transactions
+        </div>
+
+        <RightSidebar user={loggedIn} transactions = {[]} banks ={[{currentBalance:151},{currentBalance:122}]}/>
+      </section>
+
+
+
+    
   )
 }
 
